7d6e77f64c20f9f68222d6e2a25771c3207f2f86
Christopher <conrad317@gmail.com>
2018-04-17 17:49:37 -0700

    All three buttons produce random images

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var productButton1  =document.getElementById('product-button-1');
 var productButton2 = document.getElementById('product-button-2');
 var productButton3 = document.getElementById('product-button-3');
 
+var mallImg1 = document.getElementById('mall-img-1');
+var mallImg2 = document.getElementById('mall-img-2');
+var mallImg3 = document.getElementById('mall-img-3');
+
 function Pictures(url) {
   this.url = url;
   this.votes = 0;
@@ -17,7 +21,7 @@ var allPictures = [
   new Pictures('images/breakfast.jpg'),
   new Pictures('images/bubblegum.jpg'),
   new Pictures('images/chair.jpg'),
-  new Pictures('images/cthulu.jpg'),
+  new Pictures('images/cthulhu.jpg'),
   new Pictures('images/dog-duck.jpg'),
   new Pictures('images/dragon.jpg'),
   new Pictures('images/pen.jpg'),
@@ -37,12 +41,27 @@ var button3 = allPictures[2];
 
 productButton1.addEventListener('click', function(e){
   button1.votes++;
+  pickNewPictures();
+
 });
 
 productButton2.addEventListener('click', function (e) {
   button2.votes++;
+  pickNewPictures();
 });
 
 productButton3.addEventListener('click', function (e) {
   button3.votes++;
+  pickNewPictures();
 });
+
+function pickNewPictures() {
+  button1 = allPictures[Math.floor(Math.random() * allPictures.length)];
+  mallImg1.src = button1.url;
+
+  button2 = allPictures[Math.floor(Math.random() * allPictures.length)];
+  mallImg2.src = button2.url;
+
+  button3 = allPictures[Math.floor(Math.random() * allPictures.length)];
+  mallImg3.src = button3.url;
+}
